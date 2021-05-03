6686582afebe4184804292a38bb21873c797e1c5
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-05-02 17:53:41 -0700

    adjusted home screen p tag

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,6 +9,7 @@ function Home() {
           <p className="about-me-p-tag">Alexander Giannini is currently a student at the UCLA Extension Bootcamp. He also has a day job working as a QA Engineer at GOAT Group.</p>
           <p className="about-me-p-tag">After the coding bootcamp is complete, he would love to start applying to junior developer jobs and hopefully gain an opportunity there!</p>
           <p className="about-me-p-tag">He currently resides in Los Angeles, California. He went to the University of the Pacific for undergrad.</p>
+          <p className="about-me-p-tag">Alexander is an avid sports fan and he loves hanging out with family, friends, and loved ones.</p>
         </section>
       );
   }
